0fd88765977b7b2a7040d574157cc9ad0168f4ea
Mirvhin <mirvhin143@github.com>
2024-10-12 22:18:45 +0800

    Update to bind to Heroku port

--- src/main/java/com/gcash/parcel/ParcelApiApplication.java
+++ src/main/java/com/gcash/parcel/ParcelApiApplication.java
@@ -1,5 +1,7 @@
 package com.gcash.parcel;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,9 +10,15 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class ParcelApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ParcelApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(ParcelApiApplication.class);
+        String port = System.getenv("PORT");
+        if (port != null) {
+            app.setDefaultProperties(Collections.singletonMap("server.port", port));
+        }
+        app.run(args);
+    }
+
 	@Bean
 	public RestTemplate restTemplate() {
 			return new RestTemplate();
